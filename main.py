c47a81f8e128c0dca071ac1be4aeb3b3f8cefbde
Stephan Riley <sriley121991@hotmail.com>
2025-01-19 16:48:56 -0700

    draw astroids and have them move around the screen

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 
@@ -12,12 +14,18 @@ def main():
     dt = 0
     black = (0,0,0)
     pygame.init()
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
+    astroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
